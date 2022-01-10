62522603ae8acf57253c9e85f853714d14f8086b
Meera Keshattiwar <92295422+Meera12kesh@users.noreply.github.com>
2022-01-10 07:54:31 +0530

    Add files via upload

---
+++ book_demo.js
@@ -0,0 +1,11 @@
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+const btn = document.querySelector(".btn")
